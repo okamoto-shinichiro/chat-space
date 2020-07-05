da4ec71d5f15e66015b11ee22262e06a56524c13
オカモト　シンイチロウ <o75.okamoto.shinichiro@gmail.com>
2020-07-05 10:40:00 +0900

    メッセージ送信機能実装完了

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -19,12 +19,17 @@ class GroupsController < ApplicationController
 
   def edit
     @group = Group.find(params[:id])
+    # if @group.edit(group_params)
+    #   redirect_to group_messages_path, notice: 'グループを編集しました'
+    # else
+    #   render :index
+    # end
   end
 
   def update
     @group = Group.find(params[:id])
     if @group.update(group_params)
-      redirect_to root_path, notice: 'グループを更新しました'
+      redirect_to group_messages_path(@group), notice: 'グループを更新しました'
     else
       render :edit
     end
